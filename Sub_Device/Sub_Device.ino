f2d6c11de9ca40115b159c783347874bb24e47ff
LeeGueTae <bigstar1201@naver.com>
2020-01-13 14:49:04 +0900

    Hanium Arduino Code

--- Sub_Device/Sub_Device.ino
+++ Sub_Device/Sub_Device.ino
@@ -1,12 +1,12 @@
 // Sub Device 2 : Arduino Uno Board Code
 
 #include <DHT.h>        // DHT11 온습도 센서 라이브러리 불러옴
-#include <Adafruit_GFX.h> // 미세먼지 센서 라이브러리
-#include <Adafruit_PCD8544.h> // 미세먼지 센서 라이브러리 불러옴
+#include <Adafruit_GFX.h> 
+#include <Adafruit_PCD8544.h> 
 #include <SoftwareSerial.h>
 
-#define DHTPIN 2            //DHT핀을 2번 핀으로 정의 (DHT라이브러리의 매개변수로 사용),(DATA핀)
-#define DHTTYPE DHT11      //DHT타입을 DHT11로 정의 (DHT라이브러리의 매개변수로 사용)
+#define DHTPIN 2     
+#define DHTTYPE DHT11    
 
 #define BT_Rx 4
 #define BT_Tx 5
@@ -18,14 +18,14 @@
 #define delta_time 40
 #define sleep_time 9680
 
-DHT dht(DHTPIN, DHTTYPE);  //DHT설정 = dht(디지털2, dht11)  (객체생성이라 보면 됨)
+DHT dht(DHTPIN, DHTTYPE);
 
 SoftwareSerial BTserial(BT_Rx,BT_Tx);
 
 float dust_measure;
 float dust_density;
 
-Adafruit_PCD8544 display = Adafruit_PCD8544(8, 9, 10, 11, 12);    //GLCD 설정
+Adafruit_PCD8544 display = Adafruit_PCD8544(8, 9, 10, 11, 12);  
 // pin 8 - 시리얼 클럭 출력핀 (SCLK)
 // pin 9 - 시리얼 데이터 출력핀 (DIN)
 // pin 10 - 데이터/커맨드 설정핀 (D/C)
@@ -48,29 +48,29 @@ void dust_calc(){
 
 void setup()   
 {
-  Serial.begin(9600);        // 시리얼 통신을 사용하기 위해 보드레이트를 9600으로 설정합니다.
+  Serial.begin(9600);    
   BTserial.begin(9600);
   
-  dht.begin();                //DHT 온습도 센서의 정보 전송을 시작
+  dht.begin();      
   
   pinMode(Vo,INPUT);
-  pinMode(V_LED,OUTPUT);   // ledPower를 출력 단자로 설정합니다.
+  pinMode(V_LED,OUTPUT);  
   
-  display.begin();            //GLCD의 출력 시작
-  display.setContrast(50);    //디스플레이 대조 설정(반전정도 설정)
-  display.clearDisplay();     //디스플레이 클리어(디스플레이 전에 반드시 전의 것을 지워줘야 함)
+  display.begin();           
+  display.setContrast(50);   
+  display.clearDisplay();   
 
-  display.setTextSize(1);           //텍스트 사이즈 설정
-  display.setTextColor(BLACK);      //텍스트 색 설정
-  display.setCursor(0,0);           //커서 위치 설정 (글이 적힐 부분 좌표 설정)
-  display.print("dust: ");          //초기 출력
-  display.print("");                  //초기 출력
-  display.setCursor(0,15);          //커서 위치 설정 (글이 적힐 부분 좌표 설정)
-  display.println("temperature: "); //초기 출력
-  display.print("");                  //초기 출력
-  display.setCursor(0,30);          //커서 위치 설정 (글이 적힐 부분 좌표 설정)
-  display.println("humidity: ");    //초기 출력
-  display.print("");                  //초기 출력
+  display.setTextSize(1);          
+  display.setTextColor(BLACK);   
+  display.setCursor(0,0);          
+  display.print("dust: ");    
+  display.print("");           
+  display.setCursor(0,15);        
+  display.println("temperature: "); 
+  display.print("");           
+  display.setCursor(0,30);         
+  display.println("humidity: ");    
+  display.print("");               
   display.display();               
 }
 
@@ -95,16 +95,16 @@ void loop()
     send_data.toCharArray(buf,send_data.length()+1);
 
 
-    display.clearDisplay();   //Apaga o buffer e o display
+    display.clearDisplay();   
     display.setTextSize(1);   
     display.setTextColor(BLACK,WHITE);  
-    display.setCursor(0,0);  //Seta a posicao do cursor
+    display.setCursor(0,0);  
     display.println("dust: ");
     display.print(dust_density);
-    display.setCursor(0,15);  //Seta a posicao do cursor
+    display.setCursor(0,15);  
     display.println("temperature: ");
     display.print(temp);
-    display.setCursor(0,30);  //Seta a posicao do cursor
+    display.setCursor(0,30); 
     display.println("humidity: ");
     display.print(humi);
     display.display();
