3dd5a68c3756bc811aedbf1a2d2d343ba5e6368b
FSDP0 <54497598+FSDP0@users.noreply.github.com>
2020-01-13 14:54:07 +0900

    Update MEGA2560_PHPoc.ino

--- MEGA2560_PHPoc/MEGA2560_PHPoc.ino
+++ MEGA2560_PHPoc/MEGA2560_PHPoc.ino
@@ -1,16 +1,16 @@
 // Main Device Arduino Mega2560 Board Code
 
-#include <DHT.h>        // DHT11 온습도 센서 라이브러리 불러옴
+#include <DHT.h>    
 #include <SPI.h>
 #include <Phpoc.h>
 #include <SoftwareSerial.h>
-#include <Adafruit_GFX.h> // 미세먼지 센서 라이브러리
-#include <Adafruit_PCD8544.h> // 미세먼지 센서 라이브러리 불러옴
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
 
-#define DHTPIN 2            //DHT핀을 2번 핀으로 정의 (DHT라이브러리의 매개변수로 사용),(DATA핀)
-#define DHTTYPE DHT11      //DHT타입을 DHT11로 정의 (DHT라이브러리의 매개변수로 사용)
+#define DHTPIN 2         
+#define DHTTYPE DHT11     
 
-#define sub1_rx 10        //
+#define sub1_rx 10     
 #define sub1_tx 11
 
 #define sub2_rx 12
@@ -29,14 +29,14 @@
 SoftwareSerial sub_1(sub1_rx,sub1_tx);
 SoftwareSerial sub_2(sub2_rx,sub2_tx);
 
-DHT dht(DHTPIN, DHTTYPE);  //DHT설정 = dht(디지털2, dht11)  (객체생성이라 보면 됨)
+DHT dht(DHTPIN, DHTTYPE); 
 
-char server_name[]="13.125.219.211";
+char server_name[]="xxx.xxx.xxx.xxx"; // Server IP Address
 
 float dust_measure;
 float dust_density;
   
-Adafruit_PCD8544 display = Adafruit_PCD8544(8, 9, 10, 11, 12);    //GLCD 설정
+Adafruit_PCD8544 display = Adafruit_PCD8544(8, 9, 10, 11, 12);   
 // pin 8 - 시리얼 클럭 출력핀 (SCLK)
 // pin 9 - 시리얼 데이터 출력핀 (DIN)
 // pin 10 - 데이터/커맨드 설정핀 (D/C)
@@ -56,37 +56,37 @@ void dust_calc(){
   delayMicroseconds(sleep_time);
 
   dust_measure = dust_measure * 5.0 / 1024.0;
-  dust_density = (dust_measure - 0.17) / 0.005;     // 센서 초기값 약 0.17
+  dust_density = (dust_measure - 0.17) / 0.005;    
 }
 
 void setup()   
 {
-  Serial.begin(9600);        // 시리얼 통신을 사용하기 위해 보드레이트를 9600으로 설정합니다.
+  Serial.begin(9600);      
   sub_1.begin(9600);
   sub_2.begin(9600); 
   
-  dht.begin();                //DHT 온습도 센서의 정보 전송을 시작
+  dht.begin();             
   
   Phpoc.begin(PF_LOG_SPI | PF_LOG_NET);
 
   pinMode(Vo,INPUT);
-  pinMode(V_LED,OUTPUT);   // ledPower를 출력 단자로 설정합니다.
+  pinMode(V_LED,OUTPUT);   
   
-  display.begin();            //GLCD의 출력 시작
-  display.setContrast(50);    //디스플레이 대조 설정(반전정도 설정)
-  display.clearDisplay();     //디스플레이 클리어(디스플레이 전에 반드시 전의 것을 지워줘야 함)
+  display.begin();          
+  display.setContrast(50);  
+  display.clearDisplay();    
 
-  display.setTextSize(1);           //텍스트 사이즈 설정
-  display.setTextColor(BLACK);      //텍스트 색 설정
-  display.setCursor(0,0);           //커서 위치 설정 (글이 적힐 부분 좌표 설정)
-  display.print("dust: ");          //초기 출력
-  display.print("");                  //초기 출력
-  display.setCursor(0,15);          //커서 위치 설정 (글이 적힐 부분 좌표 설정)
-  display.println("temperature: "); //초기 출력
-  display.print("");                  //초기 출력
-  display.setCursor(0,30);          //커서 위치 설정 (글이 적힐 부분 좌표 설정)
-  display.println("humidity: ");    //초기 출력
-  display.print("");                  //초기 출력
+  display.setTextSize(1);          
+  display.setTextColor(BLACK);      
+  display.setCursor(0,0);         
+  display.print("dust: ");         
+  display.print("");                
+  display.setCursor(0,15);         
+  display.println("temperature: "); 
+  display.print("");                
+  display.setCursor(0,30);         
+  display.println("humidity: ");  
+  display.print("");               
   display.display();               
 }
 
@@ -118,16 +118,16 @@ void loop()
     data+=(char)sub_2.read(); 
   }
 
-    display.clearDisplay();   //Apaga o buffer e o display
+    display.clearDisplay();  
     display.setTextSize(1);   
     display.setTextColor(BLACK,WHITE);  
-    display.setCursor(0,0);  //Seta a posicao do cursor
+    display.setCursor(0,0);  
     display.println("dust: ");
     display.print(dust_density);
-    display.setCursor(0,15);  //Seta a posicao do cursor
+    display.setCursor(0,15); 
     display.println("temperature: ");
     display.print(temp);
-    display.setCursor(0,30);  //Seta a posicao do cursor
+    display.setCursor(0,30);  
     display.println("humidity: ");
     display.print(humi);
     display.display();
